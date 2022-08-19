42c5c2f45df52aaa6fbd78e04842ba23ba7c6d43
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-08-19 16:03:27 +0300

    t

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -4,10 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EpamEmailValidation {
+    public static final String EMAIL_EPAM_PATTERN = "^[a-zA-Z]+_[A-Za-z0-9_-]+@epam\\.com$";
+
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_EPAM_PATTERN);
 
     public static boolean validateEpamEmail(String email) {
-        // Put your code here
+        if (email != null && !email.isEmpty() && !email.equals(" ")) {
+            Matcher matcher = pattern.matcher(email);
+            return matcher.matches();
+        }
 
+        return false;
     }
 }
 
